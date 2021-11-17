59f380e990bd998a7aee8b0e08069a5f006f9295
Jaden Turnbull <jaden.turnbull@gmail.com>
2021-11-17 16:04:04 +0200

    second commit

--- SpringInAction/src/main/java/tacos/web/DesignTacoController.java
+++ SpringInAction/src/main/java/tacos/web/DesignTacoController.java
@@ -1,12 +1,13 @@
-// tag::head[]
 package tacos.web;
 
-import java.util.Arrays;
+import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -14,68 +15,86 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.Order;
 import tacos.Taco;
+import tacos.User;
+import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
+import tacos.data.UserRepository;
 
-@Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
+@Slf4j
 public class DesignTacoController {
+  
+  private final IngredientRepository ingredientRepo;
+  
+  private TacoRepository tacoRepo;
 
-//end::head[]
+  private UserRepository userRepo;
 
-@ModelAttribute
-public void addIngredientsToModel(Model model) {
-	List<Ingredient> ingredients = Arrays.asList(
-	  new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-	  new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-	  new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-	  new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-	  new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-	  new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-	  new Ingredient("CHED", "Cheddar", Type.CHEESE),
-	  new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-	  new Ingredient("SLSA", "Salsa", Type.SAUCE),
-	  new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-	);
-	
-	Type[] types = Ingredient.Type.values();
-	for (Type type : types) {
-	  model.addAttribute(type.toString().toLowerCase(),
-	      filterByType(ingredients, type));
-	}
-}
-	
-//tag::showDesignForm[]
+  @Autowired
+  public DesignTacoController(
+        IngredientRepository ingredientRepo, 
+        TacoRepository tacoRepo,
+        UserRepository userRepo) {
+    this.ingredientRepo = ingredientRepo;
+    this.tacoRepo = tacoRepo;
+    this.userRepo = userRepo;
+  }
+
+  @ModelAttribute(name = "order")
+  public Order order() {
+    return new Order();
+  }
+  
+  @ModelAttribute(name = "design")
+  public Taco design() {
+    return new Taco();
+  }
+  
   @GetMapping
-  public String showDesignForm(Model model) {
-    model.addAttribute("design", new Taco());
+  public String showDesignForm(Model model, Principal principal) {
+    log.info("   --- Designing taco");
+    List<Ingredient> ingredients = new ArrayList<>();
+    ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+    
+    Type[] types = Ingredient.Type.values();
+    for (Type type : types) {
+      model.addAttribute(type.toString().toLowerCase(), 
+          filterByType(ingredients, type));      
+    }
+    
+    String username = principal.getName();
+    User user = userRepo.findByUsername(username);
+    model.addAttribute("user", user);
+
     return "design";
   }
 
-//end::showDesignForm[]
-
-
-//tag::processDesignValidated[]
   @PostMapping
-  public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+  public String processDesign(
+      @Valid Taco taco, Errors errors, 
+      @ModelAttribute Order order) {
+
+    log.info("   --- Saving taco");
+
     if (errors.hasErrors()) {
       return "design";
     }
 
-    // Save the taco design...
-    // We'll do this in chapter 3
-    log.info("Processing design: " + design);
+    Taco saved = tacoRepo.save(taco);
+    order.addDesign(saved);
 
     return "redirect:/orders/current";
   }
 
-//end::processDesignValidated[]
-
-//tag::filterByType[]
   private List<Ingredient> filterByType(
       List<Ingredient> ingredients, Type type) {
     return ingredients
@@ -83,8 +102,5 @@ public void addIngredientsToModel(Model model) {
               .filter(x -> x.getType().equals(type))
               .collect(Collectors.toList());
   }
-
-//end::filterByType[]
-// tag::foot[]
+  
 }
-// end::foot[]
--- SpringInAction/src/main/java/tacos/web/OrderController.java
+++ SpringInAction/src/main/java/tacos/web/OrderController.java
@@ -1,47 +1,70 @@
-// tag::baseClass[]
 package tacos.web;
 import javax.validation.Valid;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-//end::baseClass[]
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-//tag::baseClass[]
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
-import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
+import tacos.User;
+import tacos.data.OrderRepository;
 
-@Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
   
-//end::baseClass[]
-//tag::orderForm[]
+  private OrderRepository orderRepo;
+
+  public OrderController(OrderRepository orderRepo) {
+    this.orderRepo = orderRepo;
+  }
+  
   @GetMapping("/current")
-  public String orderForm(Model model) {
-    model.addAttribute("order", new Order());
+  public String orderForm(@AuthenticationPrincipal User user, 
+      @ModelAttribute Order order) {
+    if (order.getDeliveryName() == null) {
+      order.setDeliveryName(user.getFullname());
+    }
+    if (order.getDeliveryStreet() == null) {
+      order.setDeliveryStreet(user.getStreet());
+    }
+    if (order.getDeliveryCity() == null) {
+      order.setDeliveryCity(user.getCity());
+    }
+    if (order.getDeliveryState() == null) {
+      order.setDeliveryState(user.getState());
+    }
+    if (order.getDeliveryZip() == null) {
+      order.setDeliveryZip(user.getZip());
+    }
+    
     return "orderForm";
   }
-//end::orderForm[]
 
-  
-//tag::handlePostWithValidation[]
+  // tag::processOrderWithAuthenticationPrincipal[]
   @PostMapping
-  public String processOrder(@Valid Order order, Errors errors) {
+  public String processOrder(@Valid Order order, Errors errors, 
+      SessionStatus sessionStatus, 
+      @AuthenticationPrincipal User user) {
+    
     if (errors.hasErrors()) {
       return "orderForm";
     }
     
-    log.info("Order submitted: " + order);
+    order.setUser(user);
+    
+    orderRepo.save(order);
+    sessionStatus.setComplete();
+    
     return "redirect:/";
   }
-//end::handlePostWithValidation[]
-  
-//tag::baseClass[]
-  
+  // end::processOrderWithAuthenticationPrincipal[]
+
 }
-//end::baseClass[]
