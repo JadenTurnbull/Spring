4db913add6793d65db0ed62e661f7fe3e4ed8fda
Jaden Turnbull <jaden.turnbull@gmail.com>
2021-11-18 15:18:08 +0200

    fourth commit

--- Projects/ExpenseTrackerAPI/src/main/java/com/pairlearning/expensetracker/ExpenseTrackerApiApplication.java
+++ Projects/ExpenseTrackerAPI/src/main/java/com/pairlearning/expensetracker/ExpenseTrackerApiApplication.java
@@ -1,13 +1,27 @@
 package com.pairlearning.expensetracker;
 
+import com.pairlearning.expensetracker.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
+		SpringApplication.run(ExpenseTrackerApiApplication.class, args);}
+		
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
 	}
 
 }
