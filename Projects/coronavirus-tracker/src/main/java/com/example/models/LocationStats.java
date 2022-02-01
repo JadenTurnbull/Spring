5036ccf2cdcf6a97cefbb8febac3324431b34756
Jaden Turnbull <jaden.turnbull@gmail.com>
2022-02-01 10:02:39 +0200

    Corona App

---
+++ Projects/coronavirus-tracker/src/main/java/com/example/models/LocationStats.java
@@ -0,0 +1,38 @@
+package com.example.models;
+
+public class LocationStats {
+
+	private String state;
+	private String country;
+	private int latestTotalCases;
+	
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public int getLatestTotalCases() {
+		return latestTotalCases;
+	}
+	public void setLatestTotalCases(int latestTotalCases) {
+		this.latestTotalCases = latestTotalCases;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	@Override
+	public String toString() {
+		return "LocationStats{" +
+				"state='" + state + '\'' +
+				", country='" + country + '\'' +
+				", latestTotalClasses=" + latestTotalCases +
+				'}';
+	}
+}
