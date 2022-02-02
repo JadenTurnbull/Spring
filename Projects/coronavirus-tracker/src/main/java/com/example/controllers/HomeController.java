4450a2c32773edeb068e601901972fa3ebbe8a57
Jaden Turnbull <jaden.turnbull@gmail.com>
2022-02-02 12:05:14 +0200

    corona app

--- Projects/coronavirus-tracker/src/main/java/com/example/controllers/HomeController.java
+++ Projects/coronavirus-tracker/src/main/java/com/example/controllers/HomeController.java
@@ -1,21 +1,29 @@
 package com.example.controllers;
 
+import com.example.models.LocationStats;
+import com.example.services.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.services.CoronaVirusDataService;
+import java.util.List;
 
 @Controller
 public class HomeController {
-	
-	@Autowired
-	CoronaVirusDataService coronaVirusDataService;
 
-	@GetMapping("/")
-	public String home(Model model) {
-		model.addAttribute("locationStats", coronaVirusDataService.getAllStats());
-		return "home";
-	}
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
 }
